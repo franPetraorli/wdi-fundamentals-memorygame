e49287cece5775dbc8e4d5d804897aad57d3fb74
frapet <frapet@francescos-MacBook-Pro.local>
2018-05-28 13:58:15 +1000

    added functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,18 +2,19 @@ var cards = ["queen", "queen", "king", "king"];
 
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-var cardTwo = cards[1];
-
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
-
-console.log('User flipped ' + cardsInPlay[1]);
-
-if (cardsInPlay.length === 2) {
+function checkForMatch() {
   if (cardsInPlay[0] === cardsInPlay[1]) {
     alert("You found a match!");
   } else {
-    alert("Sorry");
+    alert("Sorry, try again.");
   }
 }
+
+function flipCard(cardId) {
+  console.log('User flipped ' + cards[cardId]);
+  cardsInPlay.push(cards[cardId]);
+
+  checkForMatch();
+}
+
+flipCard(2);
